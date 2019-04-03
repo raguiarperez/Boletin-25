d5f6ad39518fb617ba465449c3f0f2b51825f8be
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2019-04-03 10:36:52 +0200

    Boletin 25

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,19 @@
+
+package boletin25;
+
+/**
+ * @author rafsniper99
+ */
+public class Boletin25 {
+
+
+    public static void main(String[] args) {
+               Ventana ven = new Ventana();
+       ven.setVisible(true);
+       ven.setDefaultCloseOperation(3);
+       Ventana2 ven2 = new Ventana2();
+       ven2.setVisible(true);
+       ven2.setDefaultCloseOperation(3);
+    }
+    
+}
